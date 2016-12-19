132d990378d84e400a52faa431bf6545f8736400
Oskar Werkelin Ahlin <oswa@kth.se>
2016-12-20 00:49:13 +0100

    solve part two in O(lg_3 n) time

--- 19/part_two.cc
+++ 19/part_two.cc
@@ -1,10 +1,25 @@
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int solve(int n) {
+int solve_fast(int n) {
+    n -= 2;
+    int at = 0;
+    int pow = 1;
+    for (;;) {
+        if (at + pow*2 > n) break;
+        at += pow * 2;
+        pow *= 3;
+    }
+    n -= at;
+    if (n < pow) return n;
+    return (pow - 1) + 2 * (n - at);
+}
+
+int solve_slow(int n) {
     vector<int> v(n, 1);
     int at = 0;
     int left = n;
@@ -27,7 +42,14 @@ int solve(int n) {
 }
 
 int main() {
+/*
+    // Run some tests
+    for (int i = 2; i <= 1000; ++i) {
+        assert(solve_fast(i) == solve_slow(i));
+    }
+*/
     int n;
     cin >> n;
-    for (int i = 2; i <= n; ++i) cout << "n = " << i << ": " << solve(i) << endl;
+
+    cout << solve_fast(n)+1 << endl;
 }
